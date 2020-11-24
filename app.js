d88a47bdbe925e345bde9197573573dd2d2ec5d9
doomhammerhell <maiconrlyeh@gmail.com>
2020-11-23 23:55:32 -0300

    Adding animation to the site with GSAP

--- app.js
+++ app.js
@@ -11,3 +11,65 @@ const mobileMenu = () => {
 }
 
 menu.addEventListener('click', mobileMenu);
+
+// animation
+
+gsap.registerPlugin(ScrollTrigger)
+
+gsap.from('.hero-animation', {
+    duration: 0.6,
+    opacity: 0,
+    y: -150,
+    stagger: 0.3
+});
+
+gsap.from('.services-animation', {
+    scrollTrigger: '.services-animation',
+    duration: 0.5,
+    opacity: 0,
+    x: -150,
+    stagger: 0.12
+});
+
+gsap.from('.img-animation', {
+    scrollTrigger: '.services-animation',
+    duration: 1.2,
+    opacity: 0,
+    x: -200
+});
+
+gsap.from('.associate-animation', {
+    scrollTrigger: '.associate-animation',
+    duration: 1,
+    opacity: 0,
+    y: -150,
+    stagger: 0.3,
+    delay: 0.5
+});
+
+gsap.from('.card-animation', {
+    scrollTrigger: '.card-animation',
+    duration: 1,
+    opacity: 0,
+    y: -150,
+    stagger: 0.1,
+    delay: 0.2
+});
+
+gsap.from('.team-animation', {
+    scrollTrigger: '.team-animation',
+    duration: 1,
+    opacity: 0,
+    y: -150,
+    stagger: 0.3,
+    delay: 0.2
+});
+
+gsap.from('.newsletter-animation', {
+    scrollTrigger: '.newsletter-animation',
+    duration: 1,
+    opacity: 0,
+    y: -150,
+    stagger: 0.3,
+    delay: 0.6
+});
